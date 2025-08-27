ba3e9e6526cc304d5a7454df7ae1c023dedc3878
bwhua <branhua@gmail.com>
2025-08-27 14:13:56 -0600

    Update base path in vite.config.ts and script links in index.html for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://bwhua.github.io/portfolio/', // Add base path for GitHub Pages
+	base: './', // Add base path for GitHub Pages
 	server: {
 		port: 3000,
 		open: true,
